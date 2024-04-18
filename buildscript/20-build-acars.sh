94baae7f393680a413aeb3fb0338f047594e13d2
Stanislav Lechev [0xAF] <af@0xAF.org>
2024-04-19 00:45:27 +0300

    update acarsdec build script

--- buildscript/20-build-acars.sh
+++ buildscript/20-build-acars.sh
@@ -5,18 +5,25 @@ ARCH=$(uname -m)
 OUTPUT_DIR=/usr/src/owrx-output/$ARCH/
 mkdir -p $OUTPUT_DIR
 
-apt install libxml2-dev libjansson-dev
+apt install -y libxml2-dev libjansson-dev
 
-mkdir libacars
-pushd libacars
-wget 'https://raw.githubusercontent.com/openwebrx/package-builder/master/packages/24-libacars/build.sh'
-chmod +x build.sh
-./build.sh
-cp *.deb $OUTPUT_DIR/
-dpkg -i *.deb
-popd
+# use Jakob's version of libacars
+# mkdir libacars
+# pushd libacars
+# wget 'https://raw.githubusercontent.com/openwebrx/package-builder/master/packages/24-libacars/build.sh'
+# chmod +x build.sh
+# ./build.sh
+# cp *.deb $OUTPUT_DIR/
+# dpkg -i *.deb
+# popd
 
+# or use our version in the ppa repo
+wget -O - https://luarvique.github.io/ppa/openwebrx-plus.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/openwebrx-plus.gpg
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/openwebrx-plus.gpg] https://luarvique.github.io/ppa/bookworm ./" > /etc/apt/sources.list.d/openwebrx-plus.list
+apt update
+apt install -y libacars-dev
 
+# build acarsdec
 mkdir acarsdec
 pushd acarsdec
 wget 'https://raw.githubusercontent.com/openwebrx/package-builder/master/packages/29-acarsdec/build.sh'
