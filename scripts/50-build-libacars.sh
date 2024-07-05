53d554bd6a2e16647e6fc56698536f43c0029ddd
Stanislav Lechev [0xAF] <af@0xAF.org>
2024-07-06 01:31:07 +0300

    rework acars packages to our own debian packaging from upstream

--- scripts/50-build-libacars.sh
+++ scripts/50-build-libacars.sh
@@ -5,26 +5,25 @@ set -euo pipefail
 # shellcheck disable=SC1091
 source /build.env
 
-: "${GIT_LIBACARS:=https://github.com/openwebrx/libacars-debian}"
+: "${GIT_LIBACARS:=https://github.com/szpajder/libacars}"
 
 if [ "${BUILD_LIBACARS:-}" == "y" ]; then
   apt install -y libxml2-dev libjansson-dev
+  log suc "Building libacars..."
 
-	log suc "Building libacars..."
-	git clone -b debian/bullseye "$GIT_LIBACARS"
-  pushd libacars-debian
-    gbp dch --debian-branch=debian/bullseye --snapshot --auto
-    git add debian/changelog
-    git commit -m "snapshot changelog"
-    gbp buildpackage --git-debian-branch=debian/bullseye -us -uc
-  popd
+  git clone "$GIT_LIBACARS"
+	pushd libacars
+    git checkout 3147aa0857b6d0fb8989b27445ce46278cb4bae8
+    patch -p1 < /scripts/patches/libacars-2.2.0-4.patch
+    dpkg-buildpackage -b -rfakeroot -us -uc
+	popd
 
 	# Install debs, so the next packages can use it.
 	dpkg -i libacars*.deb
 
 	# copy debs to the output folder
 	cp ./*.deb "${OUTPUT_DIR}/"
-	
+
 	# clean
-	rm -rf ./*.deb libacars-debian
+	rm -rf ./*.deb libacars
 fi
--- scripts/52-build-acarsdec.sh
+++ scripts/52-build-acarsdec.sh
@@ -5,30 +5,22 @@ set -euo pipefail
 # shellcheck disable=SC1091
 source /build.env
 
-: "${GIT_ACARSDEC:=https://github.com/openwebrx/acarsdec-debian}"
+: "${GIT_ACARSDEC:=https://github.com/TLeconte/acarsdec}"
 
 if [ "${BUILD_ACARSDEC:-}" == "y" ]; then
   apt install -y libxml2-dev libjansson-dev
 
 	log suc "Building acarsdec..."
-	git clone -b debian/bullseye "$GIT_ACARSDEC"
-  pushd acarsdec-debian
-    # for some reason while dpkg-buildpackage is running the cmake, the cmake cannot find libsndfile.so
-    # if i run cmake by hand it is ok, but if it's ran by dpkg-buildpackage, there's the problem.
-    # this forces cmake to link to libsndfile without checking if it's available
-    sed -i 's/find_library(LIBSNDFILE/set(LIBSNDFILE/' CMakeLists.txt
-
-    gbp dch --debian-branch=debian/bullseye --snapshot --auto
-    git add debian/changelog
-    git commit -a -m "snapshot changelog"
-    gbp buildpackage --git-debian-branch=debian/bullseye -us -uc --git-ignore-new || true # this can be removed once the problem above is fixed
-    EDITOR=/bin/true dpkg-source --commit . patch # this can be removed once the problem above is fixed
-    gbp buildpackage --git-debian-branch=debian/bullseye -us -uc --git-ignore-new
+	git clone "$GIT_ACARSDEC"
+  pushd acarsdec
+    git checkout 7920079b8e005c6c798bd478a513211daf9bbd25
+    patch -p1 < /scripts/patches/acarsdec-3.7.patch
+    dpkg-buildpackage -b -rfakeroot -us -uc
   popd
 
 	# copy debs to the output folder
 	cp ./*.deb "${OUTPUT_DIR}/"
 	
 	# clean
-	rm -rf ./*.deb acarsdec-debian
+	rm -rf ./*.deb acarsdec
 fi
