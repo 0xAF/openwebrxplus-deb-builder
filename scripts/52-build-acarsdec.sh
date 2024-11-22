17f297392ca815a9e0ba7d23fcc0773977854bf9
Stanislav Lechev [0xAF] <af@0xAF.org>
2024-11-22 22:57:44 +0200

    acarsdec build

--- scripts/52-build-acarsdec.sh
+++ scripts/52-build-acarsdec.sh
@@ -8,19 +8,19 @@ source /build.env
 : "${GIT_ACARSDEC:=https://github.com/TLeconte/acarsdec}"
 
 if [ "${BUILD_ACARSDEC:-}" == "y" ]; then
-  apt install -y libxml2-dev libjansson-dev
+	apt install -y libxml2-dev libjansson-dev
 
 	log suc "Building acarsdec..."
 	git clone "$GIT_ACARSDEC"
-  pushd acarsdec
-    git checkout 7920079b8e005c6c798bd478a513211daf9bbd25
-    patch -p1 < /scripts/patches/acarsdec-3.7.patch
-    dpkg-buildpackage -b -rfakeroot -us -uc
-  popd
+	pushd acarsdec
+		git checkout 7920079b8e005c6c798bd478a513211daf9bbd25
+		patch -p1 < /scripts/patches/acarsdec-3.7.patch
+		dpkg-buildpackage -b -rfakeroot -us -uc
+	popd
 
 	# copy debs to the output folder
 	cp ./*.deb "${OUTPUT_DIR}/"
-	
+
 	# clean
 	rm -rf ./*.deb acarsdec
 fi
